f82df3ee83bf216f9ad591f57e32abe9bd0a18c1
Paul Rill <paulrill2000@outlook.com>
2021-02-19 22:02:09 +0100

    Added two separate dockerfiles

--- gulpfile.js/tasks/build.js
+++ gulpfile.js/tasks/build.js
@@ -5,8 +5,7 @@ const config = require('../config').getConfig();
 
 const html = () => {
   return src(config.html.from)
-    .pipe(dest(config.html.to.dist))
-    .pipe(dest(config.html.to.deploy));
+    .pipe(dest(config.html.to.dist));
 }
 
 const css = () => {
@@ -15,16 +14,14 @@ const css = () => {
     .pipe($.minifyCss())
     .pipe($.autoprefixer())
     .pipe($.plumber())
-    .pipe(dest(config.css.to.dist))
-    .pipe(dest(config.css.to.deploy));
+    .pipe(dest(config.css.to.dist));
 }
 
 const js = () => {
   return src(config.js.from)
     .pipe($.order(config.js.order, { base: config.variables.src}))
     .pipe($.concat('app.js'))
-    .pipe(dest(config.js.to.dist))
-    .pipe(dest(config.js.to.deploy));
+    .pipe(dest(config.js.to.dist));
 }
 
 const webfonts = () => {
